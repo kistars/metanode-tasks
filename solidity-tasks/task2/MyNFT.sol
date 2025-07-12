ef639f3c76aefe6349fd48229d5ae37fa275cf6a
kistars <sunllen2014@outlook.com>
2025-07-12 20:18:26 +0800

    fix:bugs

--- solidity-tasks/task2/MyNFT.sol
+++ solidity-tasks/task2/MyNFT.sol
@@ -11,24 +11,24 @@ contract MyNFT is ERC721 {
 
     event MintNFT(address receiver, string tokenURI, uint256 tokenId);
 
-    mapping(uint256 tokenId => string tokenURI) private _tokenURIs;
+    mapping(uint256 tokenId => string tokenURI) public _tokenURIs;
 
     constructor() ERC721("SpaceshipNFT", "ssp") {}
 
-    function mintNFT(address receiver, string memory tokenURI) public returns (uint256) {
+    function mintNFT(address receiver, string memory uri) public returns (uint256) {
         _tokenIds.increment();
         uint256 currentId = _tokenIds.current();
 
         _mint(receiver, currentId);
-        _tokenURIs[currentId] = tokenURI;
+        _tokenURIs[currentId] = uri;
 
-        emit MintNFT(receiver, tokenURI, currentId);
+        emit MintNFT(receiver, uri, currentId);
         return currentId;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        uri = _tokenURIs[tokenId];
-        require(uri.length != 0, "uri cannot be null");
+        string memory uri = _tokenURIs[tokenId];
+        require(bytes(uri).length != 0, "uri cannot be null");
         return uri;
     }
 }
