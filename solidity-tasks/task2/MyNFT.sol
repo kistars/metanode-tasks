7c4c1caf6a8ba82b9aecac8fbffa96256a5a78ec
kistars <sunllen2014@outlook.com>
2025-07-12 19:59:33 +0800

    feat: update tokenuri function

--- solidity-tasks/task2/MyNFT.sol
+++ solidity-tasks/task2/MyNFT.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract MyNFT is ERC721 {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
 
     event MintNFT(address receiver, string tokenURI, uint256 tokenId);
+
+    mapping(uint256 tokenId => string tokenURI) private _tokenURIs;
 
     constructor() ERC721("SpaceshipNFT", "ssp") {}
 
@@ -18,9 +20,15 @@ contract MyNFT is ERC721URIStorage {
         uint256 currentId = _tokenIds.current();
 
         _mint(receiver, currentId);
-        _setTokenURI(currentId, tokenURI);
+        _tokenURIs[currentId] = tokenURI;
 
         emit MintNFT(receiver, tokenURI, currentId);
         return currentId;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        uri = _tokenURIs[tokenId];
+        require(uri.length != 0, "uri cannot be null");
+        return uri;
     }
 }
